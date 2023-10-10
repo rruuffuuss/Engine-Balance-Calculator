61d740963ba4ff4efc73149633f50cd65b285321
Rufus <rufusponniah@icloud.com>
2023-10-11 00:47:06 +0100

    added compute primary forces functionality

--- EBC/Engine Components/PistonAssembly.cs
+++ EBC/Engine Components/PistonAssembly.cs
@@ -29,12 +29,21 @@ namespace EBC.Engine_Components
         private float position;
 
         // the function ussed to calculate primary forces is force ∝ -(b^{2}(8a^{2}\cos(2x)+b^{2}(-4\cos(2x)+\cos(4x)+3)))/(2(\sqrt{2})(2a^{2}+b^{2}(\cos(2x)-1))^{\frac{3}{2}})-b\cos(x)
-        public void ComputePrimaryForces(float crankRotationDeg)
+        public Force ComputePrimaryForces(float crankRotationDeg)
         {
-            float crankRotationRad = crankRotationDeg * (MathF.Tau / 360);
-            float magnitude = -(MathF.Pow(_crankSlice.Length , 2) * 
-                (8 * MathF.Pow(_conRod.Length,2) * MathF.Cos(2 * crankRotationRad) + 
+            float crankRotationRad = crankRotationDeg * (MathF.Tau / 360f);
+            float magnitude = -(_crankSlice.LengthSqrd * (8f * _conRod.LengthSqrd * MathF.Cos(2f * crankRotationRad) +
+                _crankSlice.LengthSqrd * (-4f * MathF.Cos(2f * crankRotationRad) +
+                MathF.Cos(4f * crankRotationRad) + 3f)))
+                /
+                (2.828427f * MathF.Pow(2f * _conRod.LengthSqrd + _crankSlice.LengthSqrd * (MathF.Cos(2f * crankRotationRad) - 1f), 1.5f) -
+                _crankSlice.Length * MathF.Cos(crankRotationRad));
 
+            float direction = crankRotationDeg - 180f;
+
+            PrimaryForce = new Force(_direction: direction, _magnitude: magnitude);
+
+            return PrimaryForce;
         }
     }
 }
