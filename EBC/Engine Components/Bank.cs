16bf4b6052f342f68d2f3573b3aa368126e77e3f
Rufus <rufusponniah@icloud.com>
2023-10-12 23:44:43 +0100

    created compute reciprocating force function

---
+++ EBC/Engine Components/Bank.cs
@@ -0,0 +1,37 @@
+﻿using EBC.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBC.Engine_Components
+{
+    internal class Bank
+    {
+
+        private struct cylinder
+        {
+            public PistonAssembly pistonAssembly;          
+            /*note the tdc variable is actually the angle at which the crank segment is parallel to the cylinder and popinting toward it
+            in engines with cylinder offset, this is not top ddead center*/
+            public float tdc;
+        }
+
+        private float _angle;
+
+        private cylinder[] _cylinders;
+
+        public Force ComputeReciprocatingForces(float crankRotationDeg)
+        {
+            Force totalForce = new Force();
+
+            foreach(cylinder cyl in _cylinders)
+            {
+                totalForce = Force.AddForces(totalForce, cyl.pistonAssembly.ComputeReciprocatingForce(crankRotationDeg, cyl.tdc, _angle));
+            }
+
+            return totalForce;
+        }
+    }
+}
--- EBC/Engine Components/Engine.cs
+++ EBC/Engine Components/Engine.cs
@@ -1,36 +1,32 @@
 ﻿using EBC.Physics;
 using System;
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace EBC.Engine_Components
 {
     internal class Engine
     {
-        public Block[] Blocks { get; private set; }
+        private Bank[] banks;
 
         public Engine(string EngineFile)
         {
-            if (File.Exists(EngineFile))
-            {
-
-                string[] EngineData = File.ReadAllLines(EngineFile);
-
-                Blocks = new Block[EngineData.Count()];
-
-                for (int i = 0; i < EngineData.Count(); i++)
-                {
-                    Blocks[i] = new Block(EngineData[i]);
-                }
-            }
+            XmlTextReader engineXml = new XmlTextReader("Engine Designs\\3.6 VR6 24v FSI (EA390).xml");
 
         }
 
-        public Force ComputePrimaryForces(float CrankRotation)
+        public Force ComputeReciprocatingForces(float CrankRotation)
         {
-
+            Force totalForce = new Force();
+            foreach(Bank bank in banks)
+            {
+                totalForce = Force.AddForces(totalForce, bank.ComputeReciprocatingForces(CrankRotation));
+            }
+            return totalForce;
         }
     }
 }
