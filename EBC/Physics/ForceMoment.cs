fe266e5b0bc7e5fc606a87f8d19bb2cb3da9eb1d
Rufus <rufusponniah@icloud.com>
2023-10-18 20:23:02 +0100

    calculates moments from position and force components

--- EBC/Physics/ForceMoment.cs
+++ EBC/Physics/ForceMoment.cs
@@ -20,10 +20,18 @@ namespace EBC.Physics
             Components = components;
             Moments = new Vector3(position.X * Components.X, position.Y * Components.Y, position.Z * Components.Z);
         }
+        /// <summary>
+        /// can only be used for a force parallel to the crankshafts plane of rotation
+        /// </summary>
+        /// <param name="magnitude"></param>
+        /// <param name="direction"> </param>
+        /// <param name="position"></param>
         public Force(float magnitude, float direction, Vector3 position)
         {
-            Components = new Vector3(magnitude * MathF.Cos(direction * (MathF.Tau / 360f)), magnitude * MathF.Sin(direction * (MathF.Tau / 360f)), 0);
-            Moments = new Vector3(position.X * Components.X, position.Y * Components.Y, position.Z * Components.Z);
+            Components = new Vector3(magnitude * MathF.Sin(direction * (MathF.Tau / 360f)), magnitude * MathF.Cos(direction * (MathF.Tau / 360f)), 0);
+             Moments = new Vector3(position.Z * Components.Y + position.Y * Components.Z, 
+                position.X * Components.Z + position.Z * Components.X, 
+                position.X * Components.Y + position.Y * Components.Z);
         }
 
         public static Force AddForces(Force f1, Force f2)
