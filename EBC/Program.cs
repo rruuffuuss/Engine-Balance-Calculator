f01585a5d0ac9ef4a2fac82df3cf1f5a4998b842
Rufus <rufusponniah@icloud.com>
2023-10-18 20:23:10 +0100

    test program

--- EBC/Program.cs
+++ EBC/Program.cs
@@ -4,7 +4,7 @@ using EBC.Physics;
 
 Console.WriteLine("Hello, World!");
 
-Engine R36 = new Engine("D:\\.bodeine\\Engine Balance Calculator\\EBC\\EBC\\Engine Designs\\3.6 VR6 24v FSI (EA390).xml");
+Engine R36 = new Engine("D:\\.bodeine\\Engine Balance Calculator\\EBC\\EBC\\Engine Designs\\test 2 cyl.xml");
 
 while (true)
 {
@@ -14,7 +14,11 @@ while (true)
     Console.WriteLine("enter engine speed");
     float RPM = Convert.ToSingle(Console.ReadLine());
 
-    Force f = R36.ComputeReciprocatingForces(rot,RPM);
-    Console.WriteLine("Mag: " + Convert.ToString(f.Magnitude));
-    Console.WriteLine("Dir: " + Convert.ToString(f.Direction));
+    Force C = R36.ComputeCentripetalForce(rot,RPM);
+    Force R = R36.ComputeReciprocatingForces(rot, RPM);
+    Force A = R36.ComputeAllForces(rot, RPM);
+    Console.WriteLine(Convert.ToString("C" + C.Components));
+    Console.WriteLine(Convert.ToString("R" + R.Components));
+    Console.WriteLine(Convert.ToString("A" + A.Components));
+
 }
