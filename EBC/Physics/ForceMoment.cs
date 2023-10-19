64f287039cfce3d384ab1505f926e93f2968d9ef
Rufus <rufusponniah@icloud.com>
2023-10-19 17:18:44 +0100

    Switched from overloaded constructor to factory method

--- EBC/Physics/ForceMoment.cs
+++ EBC/Physics/ForceMoment.cs
@@ -15,10 +15,9 @@ namespace EBC.Physics
         public Vector3 Components { get; private set; }
         public Vector3 Moments { get; private set; }
 
-        public Force(Vector3 components, Vector3 position)
+        public static Force ForceFromComponents(Vector3 components, Vector3 position)
         {
-            Components = components;
-            calculateMoments(position);
+            return new Force(components, calculateMoments(components, position));
         }
         /// <summary>
         /// can only be used for a force parallel to the crankshafts plane of rotation
@@ -26,17 +25,27 @@ namespace EBC.Physics
         /// <param name="magnitude"></param>
         /// <param name="direction"> </param>
         /// <param name="position"></param>
-        public Force(float magnitude, float direction, Vector3 position)
+        public static Force ForceFromDirection(float magnitude, float direction, Vector3 position)
         {
-            Components = new Vector3(magnitude * MathF.Sin(direction * (MathF.Tau / 360f)), magnitude * MathF.Cos(direction * (MathF.Tau / 360f)), 0);
-            calculateMoments(position);
+            Vector3 components = new Vector3(magnitude * MathF.Sin(direction * (MathF.Tau / 360f)), magnitude * MathF.Cos(direction * (MathF.Tau / 360f)), 0);
+            return new Force(components, calculateMoments(components, position));
+        }
+        public static Force GetForceEmpty()
+        {
+            return new Force(new Vector3(), new Vector3());
         }
 
-        private void calculateMoments(Vector3 position)
+        private Force(Vector3 components, Vector3 moments)
         {
-            Moments = new Vector3(position.Z * Components.Y + position.Y * Components.Z,
-                position.X * Components.Z + position.Z * Components.X,
-                position.X * Components.Y + position.Y * Components.Z);
+            Components = components;
+            Moments = moments;
+        }
+
+        private static Vector3 calculateMoments( Vector3 components, Vector3 position)
+        {
+            return new Vector3(position.Z * components.Y + position.Y * components.Z,
+                position.X * components.Z + position.Z * components.X,
+                position.X * components.Y + position.Y * components.Z);
         }
         public static Force AddForces(Force f1, Force f2)
         {
