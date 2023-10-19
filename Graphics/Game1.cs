5135e9c1c596a3e80eb16f12f96c036f338acb71
Rufus <rufusponniah@icloud.com>
2023-10-19 23:21:11 +0100

    created monogame project for graphics

---
+++ Graphics/Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using EBC.Engine_Components;
+using EBC.Physics;
+
+namespace Graphics
+{
+    public class Game1 : Game
+    {
+        private Texture2D _whitePx;
+
+        private Force[] _forces;
+
+        //used for graphics scaling
+        private float _maxComponent;
+        private float _maxMoment;
+
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+        public Game1(Force[] forces, float maxComponent, float maxMoment)
+        {
+            //set fields
+            _forces = forces;
+            _maxComponent = maxComponent;
+            _maxMoment = maxMoment;
+
+            _graphics = new GraphicsDeviceManager(this);
+
+            //change resolution
+            _graphics.PreferredBackBufferWidth = 600;
+            _graphics.PreferredBackBufferHeight = 600;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _whitePx = Content.Load<Texture2D>("whitePx"); 
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            
+            foreach(Force force in _forces)
+            {
+                _spriteBatch.Draw
+                    (
+                    _whitePx,
+                    new Vector2
+                    (
+                        _graphics.PreferredBackBufferWidth / 2 + force.Components.X * (_graphics.PreferredBackBufferWidth / (2 * _maxComponent)),
+                        _graphics.PreferredBackBufferHeight / 2 +force.Components.Y * (_graphics.PreferredBackBufferHeight / (2 * _maxComponent))
+                    ),
+                    Color.White
+                    );
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Graphics/Program.cs
@@ -0,0 +1,36 @@
+﻿using EBC.Engine_Components;
+using EBC.Physics;
+using System.Numerics;
+
+float RPM = 1000;
+
+Vector3 MaxComponents = Vector3.Zero;
+Vector3 MaxMoments = Vector3.Zero;
+float maxComponent = 0;
+float maxMoment = 0;
+
+Engine engine = new Engine("D:\\.bodeine\\Engine Balance Calculator\\EBC\\EBC\\Engine Designs\\3.6 VR6 24v FSI (EA390).xml");
+ Force[] forces = new Force[360];
+
+
+
+for(int i = 0; i < forces.Length; i++)
+{
+    forces[i] = engine.ComputeReciprocatingForces(i, RPM);
+    int x = (int)MaxMoments.Length();
+
+    if (forces[i].Components.X > maxComponent) maxComponent = forces[i].Components.X;
+    if (forces[i].Components.Y > maxComponent) maxComponent = forces[i].Components.Y;
+    if (forces[i].Components.Z > maxComponent) maxComponent = forces[i].Components.Z;
+
+    if (forces[i].Moments.X > maxMoment) maxMoment = forces[i].Moments.X;
+    if (forces[i].Moments.Y > maxMoment) maxMoment = forces[i].Moments.Y;
+    if (forces[i].Moments.Z > maxMoment) maxMoment = forces[i].Moments.Z;
+}
+
+using var game = new Graphics.Game1(forces, maxComponent, maxMoment);
+game.Run();
+
+
+
+
