288acdba118d8835ebbb7a248f841f5dfcdb7984
Rufus <rufusponniah@icloud.com>
2023-10-26 19:39:33 +0100

    creaeted to draw engine

---
+++ Graphics/Components/EngineModel.cs
@@ -0,0 +1,29 @@
+﻿using EBC.Engine_Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphics.Components
+{  
+
+    internal class EngineModel
+    {
+        /// <summary>
+        /// [x][0] pos of crank [x][1] pos of pin [x][2] pos of piston
+        /// </summary>
+        Vector3[,] _engImage;
+
+        public EngineModel(Engine _engine)
+        {
+            //first pass-> get values and find largest value
+
+
+            //second pass-> scale
+        }
+
+
+    }
+}
